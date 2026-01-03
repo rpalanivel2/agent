4cae62b6286697fc3f1bac3fe15dbe448b5578f2
rpalanivel2 <rahul.palanivel@expleogroup.com>
2026-01-04 02:13:05 +0530

    Added Error handler for tools and dynamic system prompt

--- agent/agent.py
+++ agent/agent.py
@@ -1,6 +1,5 @@
 from langchain.agents.structured_output import ToolStrategy
 from langchain.agents import create_agent
-from langchain.agents.middleware import SummarizationMiddleware
 
 
 from models.classmodels import Context, ResponseFormat
@@ -9,24 +8,21 @@ from config.model import default_model
 from tools.tools import get_location, get_weather, retrieve_context 
 from prompts.prompts import SYSTEM_PROMPT
 from config.config import checkpointer
-from middleware.middleware import select_model
+from middleware.middleware import select_model, chat_history_summarizer, dynamic_system_prompt
+from middleware.errorHandler import tool_error_handler
 
 
 agent = create_agent(
     model=default_model,
     tools=[get_location, get_weather, retrieve_context],
-    system_prompt=SYSTEM_PROMPT,
     context_schema=Context,
     response_format=ToolStrategy(ResponseFormat),
     checkpointer=checkpointer,
     middleware=[
+        chat_history_summarizer,
         select_model,
-        SummarizationMiddleware(
-            model=default_model,
-            trigger=("tokens", 4000),
-            keep=("messages", 20),
-        )
-
+        dynamic_system_prompt,
+        tool_error_handler
     ]
 )
 
--- middleware/middleware.py
+++ middleware/middleware.py
@@ -1,11 +1,19 @@
-from langchain.agents.middleware import wrap_model_call, ModelRequest, ModelResponse
-from models.classmodels import Context 
-from config.model import basic_model, default_model
-
 from typing import Callable
+
+from langchain.agents.middleware import SummarizationMiddleware, wrap_model_call, ModelRequest, ModelResponse, dynamic_prompt
+
+from config.model import basic_model, default_model
+from prompts.prompts import SYSTEM_PROMPT
+
+chat_history_summarizer = SummarizationMiddleware(
+    model=default_model,
+    trigger=("tokens", 4000),
+    keep=("messages", 20),
+)
 
 @wrap_model_call
 def select_model(request: ModelRequest, handler: Callable[[ModelRequest], ModelResponse]) -> ModelResponse:
+    """Select Model Dynamically based on Context"""
     model_name = request.runtime.context.model_name
 
     if model_name == "gpt-4.1-mini":
@@ -14,3 +22,16 @@ def select_model(request: ModelRequest, handler: Callable[[ModelRequest], ModelR
         model = basic_model
 
     return handler(request.override(model=model))
+
+@dynamic_prompt
+def dynamic_system_prompt(request: ModelRequest) -> str:
+    """Generate system prompt based on user name"""
+    user_name = request.runtime.context.user_name
+
+    if user_name == "A":
+        return f"{SYSTEM_PROMPT} Provide your responses in a sarcastic and funny tone."
+    elif user_name == "B":
+        return f"{SYSTEM_PROMPT} Also add a small joke relevant to the user query"
+    
+    return SYSTEM_PROMPT
+
